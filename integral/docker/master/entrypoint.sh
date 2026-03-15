13269841e396ba7c4682a8567d96b47aace4d666
Mishenkov Daniil <mishenkovdaniil@mail.ru>
2026-03-15 19:38:45 +0300

    [integral] Remove const borders from docker, parse as parameter

--- integral/docker/master/entrypoint.sh
+++ integral/docker/master/entrypoint.sh
@@ -3,7 +3,9 @@ set -e
 
 : "${MASTER_PORT:=6000}"
 : "${DISCOVERY_PORT:=4000}"
+: "${INTEGRAL_LEFT:=0}"
+: "${INTEGRAL_RIGHT:=10000}"
 
 cd /app/build
 
-./master 0 100
+./master $INTEGRAL_LEFT $INTEGRAL_RIGHT
